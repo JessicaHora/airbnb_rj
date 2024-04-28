e855e5e7f30c3f5099895a6e83298cca1c2cc765
mayhem <peon@pop-os.localdomain>
2024-04-28 17:18:16 -0300

    new update

---
+++ models/mart/gold_review_perfect_ratio.sql
@@ -0,0 +1,33 @@
+with TBL as (
+    select listing_id, count(comments) as cnt_comment
+    from airbnb.int__reviews
+    group by listing_id
+),
+
+n_perfeito as (
+select 
+	ir.listing_id, 
+	TBL.cnt_comment,
+	count(*) as "n_comments_w_perfect"
+from airbnb.int__reviews ir
+join TBL on ir.listing_id = TBL.listing_id 
+where 
+ir.comments ilike '%perfect%' or ir.comments ilike '%perfeito%'
+group by 
+ir.listing_id, TBL.cnt_comment
+order by 
+count(*) desc
+
+),
+final as (
+	select 
+	listing_id,
+	cnt_comment,
+	n_comments_w_perfect,
+	round(n_comments_w_perfect::numeric / cnt_comment * 100, 2) as percentage
+	from n_perfeito
+	where cnt_comment >= 10
+	order by percentage DESC
+)
+
+select * from final
